2f1ee2c0a74b2c1e90b46fe7f9f1ebbb78754e38
zhanglei103 <zhanglei103@baidu.com>
2021-09-11 16:14:32 +0800

    修改indent

--- modules/indent-blackline.lua
+++ modules/indent-blackline.lua
@@ -21,6 +21,35 @@ require("indent_blankline").setup {
         "IndentBlanklineIndent5",
         "IndentBlanklineIndent6",
     },
+    filetype_exclude = {
+        "startify",
+        "dashboard",
+        "dotooagenda",
+        "log",
+        "fugitive",
+        "gitcommit",
+        "packer",
+        "vimwiki",
+        "markdown",
+        "json",
+        "txt",
+        "vista",
+        "help",
+        "todoist",
+        "NvimTree",
+        "peekaboo",
+        "git",
+        "TelescopePrompt",
+        "undotree",
+        "flutterToolsOutline",
+        "" -- for all buffers without a file type
+    },
+    buftype_exclude = {
+        "terminal",
+        "nofile"
+    }
+
 }
+
 --   -- because lazy load indent-blankline so need readd this autocmd
 vim.cmd('autocmd CursorMoved * IndentBlanklineRefresh')
